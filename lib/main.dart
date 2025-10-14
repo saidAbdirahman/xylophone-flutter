b5e5b807af85fa78e4fe881864479b45d4cb23cc
saidAbirahman <saidqaaje11@gmail.com>
2025-10-14 16:29:48 +0300

    Remove english package
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +9,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(nouns.last)),
+          child: Center(child: Text('text')),
         ),
       ),
     );
