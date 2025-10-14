4881f52c9d577f2f48e2f140456f4ce0c0fc96c5
saidAbirahman <saidqaaje11@gmail.com>
2025-10-14 16:55:12 +0300

    Play audio in asset folder
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,13 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: null,
+            child: TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                 player.play(AssetSource('note1.wav'));
+              },
+              child: Text("text"),
+            ),
           ),
         ),
       ),
